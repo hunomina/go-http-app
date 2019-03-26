658ce7d24e026a338e5cb9b59a9b6f2a447bc0a2
hunomina <hugo.f972@gmail.com>
2019-03-26 23:07:44 +0100

    Add docker composer support

--- app/main.go
+++ app/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-	"httpApplication/controller"
+	"httpApplication/app/controller"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	// catch every request not caught
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		log.Println(request.Method + " : " + request.RequestURI)
+	})
+
 	http.HandleFunc("/hello", controller.HomeController{}.Hello)
 	http.HandleFunc("/css/", controller.ResourcesController{}.GetCssFile)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
